810a5dd939665ced63521766078891231db9aa04
Sujoy De <sujoy.de345@gmail.com>
2023-09-27 14:38:01 +0530

    add user model

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Pass, SecNotes, Cards, Files
+from .models import Pass, SecNotes, Cards, Files, Users
 
 
 class PassSerializer(serializers.ModelSerializer):
@@ -22,3 +22,9 @@ class FilesSerializer(serializers.ModelSerializer):
         model = Files
         fields = '__all__'
 
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+        
+    
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializer import PassSerializer,SecNotesSerializer,CardsSerializer, FilesSerializer
-from .models import Pass,SecNotes,Cards, Files
+from rest_framework import viewsets,response
+from .serializer import PassSerializer,SecNotesSerializer,CardsSerializer, FilesSerializer, UsersSerializer
+from .models import Pass,SecNotes,Cards, Files, Users
 # Create your views here.
+
+Response = response.Response
 
 class PassViewSet(viewsets.ModelViewSet):
     queryset = Pass.objects.all()
     serializer_class = PassSerializer
+    def list_user_pass(self, request, pk=None):
+        queryset = Pass.objects.filter(owner_id=pk)
+        serializer = PassSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
 
 class SecNotesViewSet(viewsets.ModelViewSet):
     queryset = SecNotes.objects.all()
     serializer_class = SecNotesSerializer
+    def list_user_notes(self, request, pk=None):
+        queryset = SecNotes.objects.filter(owner_id=pk)
+        serializer = SecNotesSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class CardsViewSet(viewsets.ModelViewSet):
     queryset = Cards.objects.all()
     serializer_class = CardsSerializer
+    def list_user_cards(self, request, pk=None):
+        queryset = Cards.objects.filter(owner_id=pk)
+        serializer = CardsSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class FilesViewSet(viewsets.ModelViewSet):
     queryset = Files.objects.all()
     serializer_class = FilesSerializer
+    def list_user_files(self, request, pk=None):
+        queryset = Files.objects.filter(owner_id=pk)
+        serializer = FilesSerializer(queryset, many=True)
+        return Response(serializer.data)
 
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
