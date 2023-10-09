6fe050f73a3333bc14e3debcf1b93ef7550bd087
Pragy <pragyjha0502@gmail.com>
2023-10-09 21:21:56 +0530

    added cors

--- middleend/routes/auth.js
+++ middleend/routes/auth.js
@@ -2,42 +2,59 @@ const express = require("express");
 const axios = require("axios");
 const User = require("../models/user");
 const { BACKEND_URL } = require("../connection");
+const cors = require("cors");
 
-const router = express.Router();
-router.use(express.urlencoded({ extended: true }));
+const app = express();
 
-router.route("/register").post(async (req, res) => {
-  const { username, password } = req.body;
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
   console.log(username);
   console.log(password);
+  console.log(email);
   const response = await axios.post(`${BACKEND_URL}/api/users/add/`, {
     username: username,
   });
-  const user = new User({userID: response.data.id, username: username, password: password});
+  const user = new User({
+    userID: response.data.id,
+    username: username,
+    email: email,
+    password: password,
+  });
   user.save();
   var options = {
-    method: 'POST',
+    method: "POST",
     url: `${BACKEND_URL}/api/genkeypass/`,
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    data: {username: username, password: password}
+    data: { username: username, password: password },
   };
-  const tokenResponse = await axios.request(options).catch(function (error) {
-    console.error(error);
-  });
+  const tokenResponse = await axios
+    .request(options)
+    .catch(function (error) {
+      console.error(error);
+    });
   const token = tokenResponse.data.keypass;
   res.send(`User registered with token: ${token}`);
 });
 
-router.route("/login").post(async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username: username, password: password });
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  const user = await User.findOne({ email: email, password: password });
   if (user) {
     res.send(`User logged in!`);
+    console.log(200);
   } else {
     res.status(404).send(`User not found!`);
+    console.log(404);
   }
 });
 
-module.exports = router;
+module.exports = app;
