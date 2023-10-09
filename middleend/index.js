00d72d7dcee59643b57310c327fc8d7b99705be3
Sujoy De <99689289+desujoy@users.noreply.github.com>
2023-10-09 15:50:42 +0530

    middleend auth

--- middleend/index.js
+++ middleend/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const mongoose = require('mongoose');
-const User = require('./models/user');
+const auth = require('./routes/auth');
 const { MONGO_URL, PORT, connectMongo } = require('./connection');
 
 const app = express();
@@ -12,9 +13,7 @@ connectMongo(MONGO_URL).then(() => {
     console.log('Error connecting to MongoDB', err);
   });
 
-app.post('/resister', async (req, res) => {
-    
-})
+app.use('/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
--- middleend/routes/auth.js
+++ middleend/routes/auth.js
@@ -1,16 +1,43 @@
-const express = require('express');
-const axios = require('axios');
-const User = require('./models/user');
+const express = require("express");
+const axios = require("axios");
+const User = require("../models/user");
+const { BACKEND_URL } = require("../connection");
 
-const app = express();
-app.use(express.json());
+const router = express.Router();
+router.use(express.urlencoded({ extended: true }));
 
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    const response = await axios.post(`${BACKEND_URL}/register`, { username, password });
-    const user = new User({ username, password });
-    await user.save();
-
-    res.send('User created');
+router.route("/register").post(async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+  const response = await axios.post(`${BACKEND_URL}/api/users/add/`, {
+    username: username,
+  });
+  const user = new User({userID: response.data.id, username: username, password: password});
+  user.save();
+  var options = {
+    method: 'POST',
+    url: `${BACKEND_URL}/api/genkeypass/`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {username: username, password: password}
+  };
+  const tokenResponse = await axios.request(options).catch(function (error) {
+    console.error(error);
+  });
+  const token = tokenResponse.data.keypass;
+  res.send(`User registered with token: ${token}`);
 });
 
+router.route("/login").post(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username: username, password: password });
+  if (user) {
+    res.send(`User logged in!`);
+  } else {
+    res.status(404).send(`User not found!`);
+  }
+});
+
+module.exports = router;
